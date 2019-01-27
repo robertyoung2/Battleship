4c69d1d9124c92a2e2216641052e9f2220d27f0e
Robert Young <robert.young.ie@gmail.com>
2019-01-27 16:46:27 +0000

    Empty ocean map array created and displayed.

--- src/battleship.java
+++ src/battleship.java
@@ -1,2 +1,26 @@
 public class battleship {
+    public static void main(String args[]) {
+        // Create the ocean map using a single 2D array
+        String[][] ocean = new String[10][10];
+
+        // Create the numbers on the grid
+        System.out.println("**** Welcome to the Battleships Game ****");
+        System.out.println();
+        System.out.println("Right now the sea is empty.");
+        System.out.println();
+        System.out.println("  0123456789  ");
+        for (int row = 0; row < ocean.length; row++) {
+            System.out.print(row + "|");
+            for (int col = 0; col < ocean[row].length; col++) {
+                if (ocean[row][col] == null) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(ocean[row][col]);
+                }
+            }
+            System.out.println("|" + row);
+        }
+        System.out.println("  0123456789  ");
+
+    }
 }
